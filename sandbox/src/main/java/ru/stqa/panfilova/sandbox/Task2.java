4f2188c41aaffa1b1f9207f5a92b4d526483855b
NatalyaP161 <Natalya.Panfilova@tele2.ru>
2017-08-14 15:37:44 +0300

    Добавлен метод в классе Point

--- sandbox/src/main/java/ru/stqa/panfilova/sandbox/Task2.java
+++ sandbox/src/main/java/ru/stqa/panfilova/sandbox/Task2.java
@@ -3,18 +3,8 @@ package ru.stqa.panfilova.sandbox;
 public class Task2 {
 
     public static void main(String[] args) {
-
-        Point p1 = new Point();
-        Point p2 = new Point();
-        p1.x = 1;
-        p1.y = 5;
-        p2.x = 10;
-        p2.y = 4;
-        System.out.println("Расстояние между точкой (" + p1.x + "," + p1.y + ") и точкой (" + p2.x + "," + p2.y + ") = " + distance(p1,p2));
-    }
-
-    public static double distance(Point p1,Point p2) {
-
-        return Math.sqrt(Math.pow((p2.x - p1.x),2) + Math.pow((p2.y - p1.y),2));
+        Point p1 = new Point(1,5);
+        Point p2 = new Point(10,4);
+        System.out.println("Расстояние между точкой (" + p1.x + "," + p1.y + ") и точкой (" + p2.x + "," + p2.y + ") = " + p1.distance(p2.x,p2.y));
     }
 }
