08a60ba6fe39a61e7e96364a7112c9bd8916020b
NatalyaP161 <Natalya.Panfilova@tele2.ru>
2017-08-14 16:36:07 +0300

    Созданы классы Square и Rectangle, доработана MyFirstProgram

--- sandbox/src/main/java/ru/stqa/panfilova/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/panfilova/sandbox/MyFirstProgram.java
@@ -4,5 +4,11 @@ public class MyFirstProgram {
 
 	public static void main(String[] args) {
 		System.out.println("Hello, World!");
+
+		Square s = new Square(6);
+		System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
+
+		Rectangle r = new Rectangle (11,5);
+		System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
 	}
 }
---
+++ sandbox/src/main/java/ru/stqa/panfilova/sandbox/Square.java
@@ -0,0 +1,13 @@
+package ru.stqa.panfilova.sandbox;
+
+public class Square {
+    public double l;
+
+    public Square(double l) {
+        this.l = l;
+    }
+
+    public double area() {
+        return this.l * this.l;
+    }
+}
