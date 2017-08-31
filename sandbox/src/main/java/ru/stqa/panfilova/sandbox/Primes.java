11fe623f6208b3097ac998ea2f2f816fb629c321
NatalyaP161 <Natalya.Panfilova@tele2.ru>
2017-08-31 14:44:18 +0300

    Добавлен класс Primes и тесты к нему

---
+++ sandbox/src/main/java/ru/stqa/panfilova/sandbox/Primes.java
@@ -0,0 +1,15 @@
+package ru.stqa.panfilova.sandbox;
+
+public class Primes {
+
+    public static boolean isPrime(int n) {
+
+        int m = (int) Math.sqrt(n);
+        for (int i = 2; i < m; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
