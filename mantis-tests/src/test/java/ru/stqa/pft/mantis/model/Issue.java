2139ddb9e7b595372b7bedf66845020655da300a
NatalyaP161 <Natalya.Panfilova@tele2.ru>
2017-10-30 14:16:09 +0300

    Добавлена проверка бага в начале теста testGetProgects

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/model/Issue.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/model/Issue.java
@@ -6,6 +6,7 @@ public class Issue {
     private String summary;
     private String description;
     private Project project;
+    private String status;
 
     public int getId() {
         return id;
@@ -42,4 +43,14 @@ public class Issue {
         this.project = project;
         return this;
     }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public Issue withStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
 }
--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -1,12 +1,19 @@
 package ru.stqa.pft.mantis.tests;
 
+import biz.futureware.mantis.rpc.soap.client.IssueData;
+import biz.futureware.mantis.rpc.soap.client.MantisConnectPortType;
 import org.openqa.selenium.remote.BrowserType;
+import org.testng.SkipException;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
 import ru.stqa.pft.mantis.appmanager.ApplicationManager;
 
+import javax.xml.rpc.ServiceException;
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
+import java.net.MalformedURLException;
+import java.rmi.RemoteException;
 
 
 public class TestBase {
@@ -23,6 +30,25 @@ public class TestBase {
     public void tearDown() throws IOException {
         app.ftp().restore("config_inc.php.bak", "config_inc.php");
         app.stop();
+    }
+
+    public static boolean isIssueOpen(int issueId) throws MalformedURLException, ServiceException, RemoteException {
+        MantisConnectPortType mc = app.soap().getMantisConnect();
+        IssueData issueData = new IssueData();
+        issueData = mc.mc_issue_get(app.getProperty("soap.adminLogin"), app.getProperty("soap.adminPassword"), BigInteger.valueOf(issueId));
+        System.out.println(issueData.getStatus());
+        if (issueData.getStatus().getName() != "closed") {
+            return true;
+        }
+        return false;
+    }
+
+    public void skipIfNotFixed(int issueId) throws RemoteException, ServiceException, MalformedURLException {
+        if (isIssueOpen(issueId)) {
+            String textException = "Ignored because of issue " + issueId;
+            System.out.println(textException);
+            throw new SkipException(textException);
+        }
     }
 }
 
